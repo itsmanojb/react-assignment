610e41e34106cf2f73c73140e21b694549587e2b
Manoj Barman <the_manoj@live.com>
2025-03-22 23:59:31 +0530

    lint: code formatted

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -23,18 +23,28 @@ const ListHeader = ({ text }: { text: string }) => {
         backgroundColor: '#eee',
         border: (theme) => `1px solid ${theme.palette.divider}`,
         borderRadius: 2,
-        marginBlock: (theme) => theme.spacing(4, 2)
+        marginBlock: (theme) => theme.spacing(4, 2),
       }}
     >
-      <Typography variant="body1" fontWeight={500}>{text}</Typography>
+      <Typography variant="body1" fontWeight={500}>
+        {text}
+      </Typography>
       <IconButton>
-      <ModeEditOutlinedIcon />
+        <ModeEditOutlinedIcon />
       </IconButton>
     </Box>
   );
 };
 
-const ListItem = ({ label, value, icon }: { label: string; value: string, icon: React.ReactNode }) => {
+const ListItem = ({
+  label,
+  value,
+  icon,
+}: {
+  label: string;
+  value: string;
+  icon: React.ReactNode;
+}) => {
   return (
     <Box
       sx={{
@@ -78,33 +88,74 @@ export default function InfoPanel() {
       open={selectedUser !== null}
       onClose={() => {}}
     >
-      {selectedUser && (<>
-        <Box sx={{ width: panelWidth, py: 2 }}>
-        <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between',  padding: (theme) => theme.spacing(4,4,0,4) }}>
-          <Typography variant="h6">{selectedUser.fullName}</Typography>
-          <IconButton
-            aria-label="close panel"
-            size="small"
-            onClick={() => selectUser(null)}
-          >
-            <CloseIcon fontSize="inherit" />
-          </IconButton>
-        </Box>
-      
-          <Box px={4} pb={4}>
-            <ListHeader text="User Information" />
-            <ListItem label="Name" value={selectedUser.fullName} icon={<PersonOutlineOutlinedIcon/>} />
-            <ListItem label="Email" value={selectedUser.email} icon={<EmailOutlinedIcon />} />
-            <ListItem label="Department" value={selectedUser.department} icon={<BusinessOutlinedIcon />} />
-            <ListItem label="Position" value={selectedUser.designation}  icon={<BusinessCenterOutlinedIcon />} />
-            <ListItem label="Note" value={selectedUser.notes}  icon={<ModeEditOutlinedIcon />} />
-            <ListItem label="Address" value={selectedUser.address}  icon={<HomeOutlinedIcon/>} />
-            <ListItem label="Language" value={selectedUser.language}  icon={<TranslateOutlinedIcon />} />
-            <ListHeader text="Login Information" />
-            <ListItem label="Username & Password" value={selectedUser.username}  icon={<PasswordOutlinedIcon />} />
+      {selectedUser && (
+        <>
+          <Box sx={{ width: panelWidth, py: 2 }}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                padding: (theme) => theme.spacing(4, 4, 0, 4),
+              }}
+            >
+              <Typography variant="h6">{selectedUser.fullName}</Typography>
+              <IconButton
+                aria-label="close panel"
+                size="small"
+                onClick={() => selectUser(null)}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            </Box>
+
+            <Box px={4} pb={4}>
+              <ListHeader text="User Information" />
+              <ListItem
+                label="Name"
+                value={selectedUser.fullName}
+                icon={<PersonOutlineOutlinedIcon />}
+              />
+              <ListItem
+                label="Email"
+                value={selectedUser.email}
+                icon={<EmailOutlinedIcon />}
+              />
+              <ListItem
+                label="Department"
+                value={selectedUser.department}
+                icon={<BusinessOutlinedIcon />}
+              />
+              <ListItem
+                label="Position"
+                value={selectedUser.designation}
+                icon={<BusinessCenterOutlinedIcon />}
+              />
+              <ListItem
+                label="Note"
+                value={selectedUser.notes}
+                icon={<ModeEditOutlinedIcon />}
+              />
+              <ListItem
+                label="Address"
+                value={selectedUser.address}
+                icon={<HomeOutlinedIcon />}
+              />
+              <ListItem
+                label="Language"
+                value={selectedUser.language}
+                icon={<TranslateOutlinedIcon />}
+              />
+              <ListHeader text="Login Information" />
+              <ListItem
+                label="Username & Password"
+                value={selectedUser.username}
+                icon={<PasswordOutlinedIcon />}
+              />
+            </Box>
           </Box>
-      </Box>
-      </> )}
+        </>
+      )}
     </Drawer>
   );
 }
