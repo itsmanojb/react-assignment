51cc09ffde55f6b0944b300e705cababb8af4432
Suvadip Ghosh <dip.suvadip@gmail.com>
2025-03-23 09:56:58 +0100

    add more tests

--- src/hooks/useFetch.test.tsx
+++ src/hooks/useFetch.test.tsx
@@ -1,6 +1,7 @@
-//import { renderHook, waitFor } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import fetchMock from 'jest-fetch-mock';
-//import useFetch from './useFetch';
+import { BASE_URL } from '../config/ApiConfig';
+import useFetch from './useFetch';
 
 //const BASE_URL = import.meta.env.VITE_API_URL || 'http://mockapi.com';
 
@@ -14,34 +15,33 @@ describe('useFetch hook', () => {
     expect(true).toBeTruthy();
   });
 
-  /*
   it('should return loading initially', async () => {
     fetchMock.mockResponseOnce(JSON.stringify({}));
     const { result } = renderHook(() => useFetch('test-endpoint'));
 
-    await waitFor(() => expect(result.current.loading).toBe(false));
-
-    expect(result.current.loading).toBe(true);
+    await waitFor(() => expect(result.current.loading).toBe(true));
   });
 
   it('should fetch data and return it', async () => {
     const mockData = { message: 'success' };
     localStorage.setItem('token', 'mock-token');
     fetchMock.mockResponseOnce(JSON.stringify(mockData));
-  
+
     const { result } = renderHook(() => useFetch('test-endpoint'));
-  
-    await waitFor(() => expect(result.current.loading).toBe(false));
-  
+
+    // Wait until data is fetched and state updates
+    await waitFor(() => expect(result.current.data).toEqual(mockData));
+
     expect(fetchMock).toHaveBeenCalledWith(`${BASE_URL}/test-endpoint`, {
       headers: {
-        Authorization: 'Bearer mock-token',
+        Authorization: `Bearer mock-token`,
       },
     });
-    expect(result.current.data).toEqual(mockData);
     expect(result.current.error).toBeNull();
+    expect(result.current.loading).toBe(false);
   });
 
+  
   it('should handle fetch errors', async () => {
     fetchMock.mockRejectOnce(new Error('Fetch failed'));
 
@@ -64,5 +64,5 @@ describe('useFetch hook', () => {
     expect(result.current.error).toBe('Error: 500 Internal Server Error');
     expect(result.current.data).toBeNull();
     expect(result.current.loading).toBe(false);
-  }); */
+  });
 });
